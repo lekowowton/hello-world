f03f92c0b7c13016616ebb9f62965f381f8119c9
LekoWowton <l.maaug@gmail.com>
2018-12-20 18:56:38 -0200

    Update lekalipi.sh

--- lekalipi.sh
+++ lekalipi.sh
@@ -1,8 +1,14 @@
 # Kali pi 3b+
 # from https://whitedome.com.au/re4son/kali-pi/
 # trying something
+
+# cloning into RPI CONFIG
+git clone https://github.com/Re4son/RPi-Tweaks.git 
+
+# installing something maybe will be necessary
 sudo apt-get install aptitude curl cmake build-essential mailutils python-dev python-pip libusb-1.0-0-dev python-numpy htop ftp locate screen kismet pure-ftpd tigervnc-standalone-server tmux default-mysql-server darkstat ntopng mana-toolkit beef-xss resolvconf mitmf dnsmasq hostapd
 
+# mount mmc... downloading and extracting the re4son kernel ..
 sudo su
 mount /dev/mmcblk0p1 /boot
 cd /usr/local/src
